dc5aa62b8250f78988a3f7b6f58e27ab43ae1ec5
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-03-06 23:11:26 +0530

    added cursor-pointer on the ban useer button in user management panel of admin dashboard

--- src/Pages/Admin/UserManagement.jsx
+++ src/Pages/Admin/UserManagement.jsx
@@ -69,7 +69,7 @@ const UserModal = ({ user, onClose }) => {
                     <button onClick={onClose} className="px-4 cursor-pointer py-2 text-gray-600 font-medium hover:bg-gray-200 rounded-lg transition-colors">
                         Close
                     </button>
-                    <button className="px-4 py-2 bg-red-600 text-white font-medium rounded-lg hover:bg-red-700 transition-colors flex items-center gap-2">
+                    <button className="px-4 cursor-pointer py-2 bg-red-600 text-white font-medium rounded-lg hover:bg-red-700 transition-colors flex items-center gap-2">
                         <Ban size={16} />
                         Ban User
                     </button>
